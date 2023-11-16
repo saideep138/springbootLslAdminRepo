7a6a9764adee2c059e91a244dbfbb8a5c80bd683
saideep138 <saideepg138@gmail.com>
2023-11-16 17:14:15 +0530

    Initial commit for springBoot LSL project code

---
+++ lsl-admin/src/main/java/com/lsl/packageoftsal/model/response/HomeResponse.java
@@ -0,0 +1,18 @@
+package com.lsl.packageoftsal.model.response;
+
+public class HomeResponse {
+	private String empName;
+	private int empid;
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+}
---
+++ lsl-demo/src/test/java/com/lsl/packageofdemotsal/LslDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsl.packageofdemotsal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LslDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
