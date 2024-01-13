1865be47832fada944c4f055f2e8aa936a11e897
saideep138 <saideepg138@gmail.com>
2024-01-13 08:26:54 +0530

    added code for pagination

--- lsl-admin/src/main/java/com/lsl/packageoftsal/controller/Conceptual/ARestHandlingConceptController.java
+++ lsl-admin/src/main/java/com/lsl/packageoftsal/controller/Conceptual/ARestHandlingConceptController.java
@@ -1,17 +1,12 @@
 package com.lsl.packageoftsal.controller.Conceptual;
 
-import java.util.List;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,23 +16,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lsl.packageoftsal.exceptionpack.LSLException;
-import com.lsl.packageoftsal.hiberanatepack.entities.CustomerInfoEntity;
-import com.lsl.packageoftsal.hiberanatepack.repositoeries.CustomerInfoRespository;
 import com.lsl.packageoftsaled.pojo.HomeRequestBody;
 import com.lsl.packageoftsaled.pojo.LslResponse;
+import com.lsl.packageoftsaled.pojo.TripResponse;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @RestController
-@RequestMapping("/paginationConcepts")
-public class PaginationConceptsController {
-
-	@Autowired
-	CustomerInfoRespository customerInfoRespository;
+@RequestMapping("/requestHandle")
+public class ARestHandlingConceptController {
 	
 
 	Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	
 	/*
 	 * 1.Pagination
@@ -46,44 +38,51 @@ public class PaginationConceptsController {
 	@ResponseBody
 	@RequestMapping(value="/fetch", method=RequestMethod.POST,consumes=MediaType.APPLICATION_JSON_VALUE,produces=MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<LslResponse> fetch(
+			
 			@RequestHeader(name="headParam") 
 			String headerVar,
+			
 			@RequestBody 
 			HomeRequestBody request,
+			
 			@RequestParam("requestParamJsonVar")
 			String requestParamVar,
+			
 			String requestParam,//@RequestParam("requestParam") is not need as json and java has same name for varible
+			
 			HttpServletRequest hreq,
-			HttpServletResponse hres) throws LSLException, Exception {
-		
-		LslResponse response = null;
-		
-		
-		
+			
+			HttpServletResponse hres
+			)
+			throws LSLException, Exception {
 		
 		
-        response = new LslResponse(solveA(), 200);
-        
 		
-
-		return new ResponseEntity<LslResponse>(response, HttpStatus.OK);
-	}
-	
-	
-	Object solveA() {
-		// First page with 5 items
-        Pageable pageble = PageRequest.of(
-            0, 5, Sort.by("customerName").ascending());
-        Page<CustomerInfoEntity> page = customerInfoRespository.findAll(pageble);
-        
-        return page.getContent();
-	}
-	
-	Object solveB() {
-		// Just sort by column
-
-        List<CustomerInfoEntity> list = customerInfoRespository.findAll(Sort.by("mobileNo"));
-        
-        return list;
+		
+		ResponseEntity<LslResponse> response = null;
+		LslResponse lslResponse = null;
+		
+		
+		
+		
+		HttpHeaders headerMap = new HttpHeaders();
+		headerMap.add("ResponseHeaderKey", "ResponseHeaderValue");
+		headerMap.add("headParam", headerVar);
+		
+		
+		try {
+			lslResponse = new LslResponse(request, 200);
+		}
+		catch(Exception e) {
+			log.info("Caught with exception");
+		}
+		
+		
+		
+		
+		
+		response = new ResponseEntity<LslResponse>(lslResponse,headerMap,HttpStatus.OK);
+		
+		return response;
 	}
 }
